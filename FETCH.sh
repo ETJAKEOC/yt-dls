3c06dedc498b2d530be1aa1f0cb81666881ac174
ETJAKEOC <etjakeoc@gmail.com>
2022-11-14 23:34:16 -0500

    modified:   FETCH.sh

--- FETCH.sh
+++ FETCH.sh
@@ -1,37 +1,39 @@
 ## Set YouTube download directory
 
 yt='/M/YOUTUBE'
+yt_flags="--no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month'"
 
 ## Change into the YouTube directory, then the channel directory (you must create your own directories for each channel, this script does NOT do that.)
 ## and then downloads the last months worth, up to 15 videos, from said channel into it's own directory, with chapter data added and sponsor segments removed.
+
 ## Example provided below for reference and cloning.
-## cd $yt/CHANNEL_DIR/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/CHANNEL_ID/videos
+## cd $yt/CHANNEL_DIR/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/CHANNEL_ID/videos
 
-cd $yt/Aging\ Wheels/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/user/agingwheels/videos
+cd $yt/Aging\ Wheels/ && yt-dlp $(yt_flags) https://www.youtube.com/user/agingwheels/videos
 
-cd $yt/Big\ Clive/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/user/bigclivedotcom/videos
+cd $yt/Big\ Clive/ && yt-dlp $(yt_flags) https://www.youtube.com/user/bigclivedotcom/videos
 
-cd $yt/Cathode Ray\ Dude/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/UCXnNibvR_YIdyPs8PZIBoEw/videos
+cd $yt/Cathode Ray\ Dude/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/UCXnNibvR_YIdyPs8PZIBoEw/videos
 
-cd $yt/Certifiably\ Ingame/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/UCJutYkYphYcGo-xbqyQJeYA/videos
+cd $yt/Certifiably\ Ingame/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/UCJutYkYphYcGo-xbqyQJeYA/videos
 
-cd $yt/Chris\ Titus\ Tech/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor -- dateafter='now-1month' https://www.youtube.com/user/homergfunk/videos
+cd $yt/Chris\ Titus\ Tech/ && yt-dlp $(yt_flags) https://www.youtube.com/user/homergfunk/videos
 
-cd $yt/Gray\ Still\ Plays/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/UCzEy7pi3B7TIS9cn_sdKK9A/videos
+cd $yt/Gray\ Still\ Plays/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/UCzEy7pi3B7TIS9cn_sdKK9A/videos
 
-cd $yt/Great\ Scott/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/user/greatscottlab/videos
+cd $yt/Great\ Scott/ && yt-dlp $(yt_flags) https://www.youtube.com/user/greatscottlab/videos
 
-cd $yt/IGP/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/TheIndieGamePromoter/videos
+cd $yt/IGP/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/TheIndieGamePromoter/videos
 
-cd $yt/MrBallen/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/UCtPrkXdtCM5DACLufB9jbsA/videos
+cd $yt/MrBallen/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/UCtPrkXdtCM5DACLufB9jbsA/videos
 
-cd $yt/Qxir/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/UCGHDQtN_vzFYJaq_Fx1eikg/videos
+cd $yt/Qxir/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/UCGHDQtN_vzFYJaq_Fx1eikg/videos
 
-cd $yt/Real\ Civil\ Engineer/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/UCeP4Yv3s4RvS0-6d9OInRMw/videos
+cd $yt/Real\ Civil\ Engineer/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/UCeP4Yv3s4RvS0-6d9OInRMw/videos
 
-cd $yt/Robert\ Murray-Smith/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/RobertMurraySmith/videos
+cd $yt/Robert\ Murray-Smith/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/RobertMurraySmith/videos
 
-cd $yt/Tech\ Ingredients/ && yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f "(bv*[vcodec~='^((he|a)vc|h26[45])']+ba) / (bv*+ba/b)" --sponsorblock-remove sponsor --dateafter='now-1month' https://www.youtube.com/channel/TechIngredients/videos
+cd $yt/Tech\ Ingredients/ && yt-dlp $(yt_flags) https://www.youtube.com/channel/TechIngredients/videos
 
 ## Change back into the main YouTube directory, and echo a timestamp into a log file, indicating when the script last updated the libraries.
 
