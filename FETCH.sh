e2f5a50a495309479dc79bb4c8b602c707c9e689
ETJAKEOC <etjakeoc@gmail.com>
2022-11-16 16:52:45 -0500

    modified:   FETCH.sh

--- FETCH.sh
+++ FETCH.sh
@@ -10,6 +10,10 @@ keep="--dateafter=now-1month" # Tells yt-dlp to only download up to one month fr
 dlp_yt="yt-dlp -w --no-playlist --playlist-end 15 --embed-metadata --write-info-json --embed-chapters -f $qual --sponsorblock-remove sponsor $keep"
 # Variable to run in place of 'yt-dlp' in script, applying the 'qual' and 'keep' flags we defined up above.
 
+## Change into the YouTube directory, and begin our log, starting with when the command first executed.
+
+cd $yt && echo YouTube download script started at: $(date) > LOG.txt # We nuke the old log, and rewrite it from scratch from this point, then append at the end of the script.
+
 ## Change into the YouTube directory, then the channel directory, and begins downloading with our defined flags.
 
 ## Example provided below for reference and cloning. (Sometimes, '/c/' must be replaced with '/user/'.)
@@ -48,9 +52,9 @@ cd $yt/TLC/ && $dlp_yt https://www.youtube.com/c/TheLinuxCast/videos
 
 cd $yt/TLE/ && $dlp_yt https://www.youtube.com/c/TheLinuxExperiment/videos
 
-## Change back into the main YouTube directory, and echo a timestamp into a log file, indicating when the script last updated the libraries.
+## Change back into the main YouTube directory, and echo a timestamp into a log file, telling us how long the script ran for.
 
-cd $yt && echo Subscriptions last updated $(date) > LOG.txt
+cd $yt && echo Subscriptions last updated $(date) >> LOG.txt
 
 ## Just a little bit of cleanup after the process, because we don't need to keep things we redownload each time.
 
