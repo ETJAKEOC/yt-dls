f867ec374c24c1634aa09141be4a2f2d6cfbd142
ETJAKEOC <etjakeoc@gmail.com>
2022-11-17 12:37:49 -0500

    modified:   FETCH.sh

--- FETCH.sh
+++ FETCH.sh
@@ -31,6 +31,8 @@ cd $yt/Certifiably\ Ingame/ && $dlp_yt https://www.youtube.com/c/CertifiablyInga
 
 cd $yt/CTT/ && $dlp_yt https://www.youtube.com/user/homergfunk/videos
 
+cd $yt/DT/ && $dlp_yt https://www.youtube.com/user/DistroTube/videos
+
 cd $yt/GSP/ && $dlp_yt https://www.youtube.com/c/GrayStillPlays/videos
 
 cd $yt/Great\ Scott/ && $dlp_yt https://www.youtube.com/c/greatscottlab/videos
