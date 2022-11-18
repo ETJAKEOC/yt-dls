780e51bcfd322c3c52246dd33980a6c267d13903
ETJAKEOC <etjakeoc@gmail.com>
2022-11-18 05:57:56 -0500

    added cleanup section to script:   FETCH.sh

--- FETCH.sh
+++ FETCH.sh
@@ -12,7 +12,8 @@ dlp_yt="yt-dlp --no-playlist --playlist-end 15 --embed-metadata --write-info-jso
 
 ## Change into the YouTube directory, and begin our log, starting with when the command first executed.
 
-cd $yt && echo YouTube download script started at: $(date) > LOG.txt # We nuke the old log, and rewrite it from scratch from this point, then append at the end of the script.
+## We nuke the old log, and rewrite it from scratch from this point, then append at the end of the script.
+cd $yt && echo YouTube download script started at: $(date) > LOG.txt
 
 ## Change into the YouTube directory, then the channel directory, and begins downloading with our defined flags.
 
@@ -47,17 +48,28 @@ cd $yt/RCE/ && $dlp_yt https://www.youtube.com/c/RealCivilEngineerGaming/videos
 
 cd $yt/RMS/ && $dlp_yt https://www.youtube.com/c/RobertMurraySmith/videos
 
-cd $yt/Tech\ Ingredients/ && yt-dlp -w --no-playlist --playlist-end 8 --embed-metadata --write-info-json --embed-chapters -f $qual --sponsorblock-remove sponsor https://www.youtube.com/c/TechIngredients/videos
+cd $yt/Tech\ Ingredients/ && yt-dlp -w --no-playlist --playlist-end 8 --embed-metadata \
+--write-info-json --embed-chapters -f $qual --sponsorblock-remove sponsor \
+https://www.youtube.com/c/TechIngredients/videos
 ## Modified for a channel that doesn't upload as often, to collect atleast 8 of their videos.
 
 cd $yt/TLC/ && $dlp_yt https://www.youtube.com/c/TheLinuxCast/videos
 
 cd $yt/TLE/ && $dlp_yt https://www.youtube.com/c/TheLinuxExperiment/videos
 
-## Change back into the main YouTube directory, and echo a timestamp into a log file, telling us how long the script ran for.
+## Change back into the main YouTube directory, and echo a timestamp into a log file
+## telling us how long the script ran for.
 
 cd $yt && echo YouTube download script completed at: $(date) >> LOG.txt
 
-## Just a little bit of cleanup after the process, because we don't need to keep things we redownload each time.
+## These next few strings are designed to help keep things tidy. The second is disabled by default.
+## First, we must cleanup temporary files and metadata that will just be rewritten when this script runs again.
 
 cd $yt && rm */*.temp.* */*.json
+
+## Then, we have a string that will clean any files older than 1 month in the downloads folder, by default.
+## You can modify this by changing the '-mtime +##' section, this takes values in number of days.
+## Recommend disabling this if possible for the first month, as there is a slight possibility it deletes
+## videos that have been downloaded which you wish to keep.
+
+#find $yt/* -mtime +30 -exec rm *.mp4
