7449c67ed19a1be0c7aa2d331bc4cb309b35fdc6
ETJAKEOC <etjakeoc@gmail.com>
2022-12-18 17:50:12 -0500

    Some re-working of cleanups:   FETCH.sh

--- FETCH.sh
+++ FETCH.sh
@@ -10,84 +10,74 @@ qual="(bv*[vcodec~='^((he|a)vc|h26[45])']+ba)/(bv*+ba/b)" # Tells yt-dlp that we
 #keep="--dateafter=now-3months" # Tells yt-dlp to only download up to 3 months from today's date worth of videos.
 dlp_yt="yt-dlp --no-playlist --playlist-end 10 --embed-metadata --write-info-json --embed-chapters -f $qual --sponsorblock-remove sponsor"
 # Variable to run in place of 'yt-dlp' in script, applying the 'qual' and 'keep' (disabled since we download only 10 videos) flags we defined up above.
-
-cleanup='rm */*.temp.* */*.json */*.meta' # This variable makes cleanup occur after downloading.
+cleanup='find ./ -type f | tail -n +11 && rm *.temp.* *.json *.meta' # This variable makes cleanup occur after downloading.
 
 ## Change into the YouTube directory, and begin our log, starting with when the command first executed.
-
 ## We nuke the old log, and rewrite it from scratch from this point, then append at the end of the script.
 echo 'YouTube download script started at:' $(date) > $yt/LOG.txt
 
 ## Change into the YouTube directory, then the channel directory, and begins downloading with our defined flags.
 
 ## Example provided below for reference and cloning. (Sometimes, '/c/' must be replaced with '/user/'.)
-## cd $yt/CHANNEL_DIR/ && $dlp_yt https://www.youtube.com/c/CHANNEL_ID/videos
+## cd $yt/CHANNEL_DIR/ && $dlp_yt https://www.youtube.com/c/CHANNEL_ID/video && $cleanup
 
-cd $yt/Aging\ Wheels/ && $dlp_yt https://www.youtube.com/user/agingwheels/videos &\
-\
-cd $yt/Big\ Clive/ && $dlp_yt https://www.youtube.com/c/Bigclive/videos &\
-\
-cd $yt/BR/ && $dlp_yt https://www.youtube.com/c/BrodieRobertson/videos &\
-\
-cd $yt/CRD/ && $dlp_yt https://www.youtube.com/c/@CathodeRayDude/videos &\
+cd $yt/Aging\ Wheels/ && $dlp_yt https://www.youtube.com/user/agingwheels/video && $cleanup &\
 
-wait
+cd $yt/Big\ Clive/ && $dlp_yt https://www.youtube.com/c/Bigclive/video && $cleanup &\
 
-cd $yt/Certifiably\ Ingame/ && $dlp_yt https://www.youtube.com/c/CertifiablyIngame/videos &\
-\
-cd $yt/CTT/ && $dlp_yt https://www.youtube.com/user/homergfunk/videos &\
-\
-cd $yt/DT/ && $dlp_yt https://www.youtube.com/c/DistroTube/videos &\
-\
-cd $yt/EE/ && $dlp_yt https://www.youtube.com/@EngineeringExplained/videos &\
+cd $yt/BR/ && $dlp_yt https://www.youtube.com/c/BrodieRobertson/video && $cleanup &\
+
+cd $yt/CRD/ && $dlp_yt https://www.youtube.com/c/@CathodeRayDude/video && $cleanup &\
 
 wait
 
-cd $yt/GSP/ && $dlp_yt https://www.youtube.com/c/GrayStillPlays/videos &\
-\
-cd $yt/Great\ Scott/ && $dlp_yt https://www.youtube.com/c/greatscottlab/videos &\
-\
-cd $yt/IGP/ && $dlp_yt https://www.youtube.com/c/IGP/videos &\
-\
-cd $yt/MF/ && $dlp_yt https://www.youtube.com/c/UndecidedMF/videos &\
+cd $yt/Certifiably\ Ingame/ && $dlp_yt https://www.youtube.com/c/CertifiablyIngame/video && $cleanup &\
+
+cd $yt/CTT/ && $dlp_yt https://www.youtube.com/user/homergfunk/video && $cleanup &\
+
+cd $yt/DT/ && $dlp_yt https://www.youtube.com/c/DistroTube/video && $cleanup &\
+
+cd $yt/EE/ && $dlp_yt https://www.youtube.com/@EngineeringExplained/video && $cleanup &\
 
 wait
 
-cd $yt/MrBallen/ && $dlp_yt https://www.youtube.com/c/MrBallen/videos &\
-\
-cd $yt/MWTB/ && $dlp_yt https://www.youtube.com/c/@Mrwhosetheboss/videos &\
-\
-cd $yt/PE/ && $dlp_yt https://www.youtube.com/c/@PracticalEngineeringChannel/videos &\
-\
-cd $yt/Qxir/ && $dlp_yt https://www.youtube.com/c/QxirYT/videos &\
+cd $yt/GSP/ && $dlp_yt https://www.youtube.com/c/GrayStillPlays/video && $cleanup &\
+
+cd $yt/Great\ Scott/ && $dlp_yt https://www.youtube.com/c/greatscottlab/video && $cleanup &\
+
+cd $yt/IGP/ && $dlp_yt https://www.youtube.com/c/IGP/video && $cleanup &\
+
+cd $yt/MF/ && $dlp_yt https://www.youtube.com/c/UndecidedMF/video && $cleanup &\
 
 wait
 
-cd $yt/RCE/ && $dlp_yt https://www.youtube.com/c/RealCivilEngineerGaming/videos &\
-\
-cd $yt/RMS/ && $dlp_yt https://www.youtube.com/c/RobertMurraySmith/videos &\
-\
-cd $yt/TC/ && $dlp_yt https://www.youtube.com/c/@TechnologyConnections/videos &\
-\
-cd $yt/Tech\ Ingredients/ && $dlp_yt https://www.youtube.com/c/TechIngredients/videos &\
+cd $yt/MrBallen/ && $dlp_yt https://www.youtube.com/c/MrBallen/video && $cleanup &\
+
+cd $yt/MWTB/ && $dlp_yt https://www.youtube.com/c/@Mrwhosetheboss/video && $cleanup &\
+
+cd $yt/PE/ && $dlp_yt https://www.youtube.com/c/@PracticalEngineeringChannel/video && $cleanup &\
+
+cd $yt/Qxir/ && $dlp_yt https://www.youtube.com/c/QxirYT/video && $cleanup &\
 
 wait
 
-cd $yt/TLC/ && $dlp_yt https://www.youtube.com/c/TheLinuxCast/videos &\
-\
-cd $yt/TLE/ && $dlp_yt https://www.youtube.com/c/TheLinuxExperiment/videos
+cd $yt/RCE/ && $dlp_yt https://www.youtube.com/c/RealCivilEngineerGaming/video && $cleanup &\
+
+cd $yt/RMS/ && $dlp_yt https://www.youtube.com/c/RobertMurraySmith/video && $cleanup &\
+
+cd $yt/TC/ && $dlp_yt https://www.youtube.com/c/@TechnologyConnections/video && $cleanup &\
+
+cd $yt/Tech\ Ingredients/ && $dlp_yt https://www.youtube.com/c/TechIngredients/video && $cleanup &\
 
 wait
 
-## Change back into the main YouTube directory, cleans up, and echos a timestamp into a log file
+cd $yt/TLC/ && $dlp_yt https://www.youtube.com/c/TheLinuxCast/video && $cleanup &\
+
+cd $yt/TLE/ && $dlp_yt https://www.youtube.com/c/TheLinuxExperiment/video && $cleanup
+
+wait
+
+## Change back into the main YouTube directory and echos a timestamp into a log file
 ## telling us how long the script ran for.
 
-cd $yt && $cleanup
 echo 'YouTube download script completed at:' $(date) >> $yt/LOG.txt
-
-## We have a string that will clean any files older than 1 month in the downloads folder, by default.
-## You can modify this by changing the '-mtime +##' section, this takes values in number of days.
-## Recommend disabling this if possible for the first month, as there is a slight possibility it deletes
-## videos that have been downloaded which you wish to keep.
-
-#find $yt/* -mtime +30 -exec rm *.*
