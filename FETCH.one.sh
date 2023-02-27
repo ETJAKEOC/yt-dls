0f43ba4bb3f0c6ccd4b1eb2eb5a903fe482545b6
ETJAKEOC <etjakeoc@gmail.com>
2023-02-27 17:07:33 +0000

    relocated directory:   FETCH.one.sh relocated directory:   FETCH.sh

--- FETCH.one.sh
+++ FETCH.one.sh
@@ -8,13 +8,13 @@ PATH=/usr/bin:/usr/sbin:usr/local/bin:usr/local/sbin:$PATH
 ## These flags by default will download up to 1 video, per channel, at full quality HD, with chapter data appened, and sponsor segments removed.
 ## This script is intended to be run daily, our hourly if you prefer, to reduce load on the machine, while keeping up to date hopefully.
 
-yt='/STORAGE/YOUTUBE' # Where the YouTube main directory is.
+yt='/STORAGE/MEDIA/YOUTUBE' # Where the YouTube main directory is.
 qual="(bv*[vcodec~='^((he|a)vc|h26[45])']+ba)/(bv*+ba/b)" # Tells yt-dlp that we want the highest quality video.
 
 dlp_yt="yt-dlp --no-playlist --playlist-end 1 --embed-metadata --write-info-json --embed-thumbnail --embed-subs --video-multistreams --audio-multistreams --embed-chapters -f $qual --sponsorblock-remove sponsor"
 # Variable to run in place of 'yt-dlp' in script, applying the 'qual' flags we defined up above.
 
-cleanup='rm -f *.temp.* *.json *.meta' # This variable makes cleanup occur after downloading.
+cleanup='rm -f *.temp.* *.json *.meta .webp' # This variable makes cleanup occur after downloading.
 
 ## Change into the YouTube directory, and begin our log, starting with when the command first executed.
 ## We nuke the old log, and rewrite it from scratch from this point, then append at the end of the script.
--- FETCH.sh
+++ FETCH.sh
@@ -7,13 +7,13 @@ PATH=/usr/bin:/usr/sbin:usr/local/bin:usr/local/sbin:$PATH
 ## Set YouTube download directory and 'yt-dlp' flags.
 ## These flags by default will download up to 10 videos, per channel, at full quality HD, with chapter data appened, and sponsor segments removed.
 
-yt='/STORAGE/YOUTUBE' # Where the YouTube main directory is.
+yt='/STORAGE/MEDIA/YOUTUBE' # Where the YouTube main directory is.
 qual="(bv*[vcodec~='^((he|a)vc|h26[45])']+ba)/(bv*+ba/b)" # Tells yt-dlp that we want the highest quality video.
 
 dlp_yt="yt-dlp --no-playlist --playlist-end 10 --embed-metadata --write-info-json --embed-thumbnail --embed-subs --video-multistreams --audio-multistreams --embed-chapters -f $qual --sponsorblock-remove sponsor"
 # Variable to run in place of 'yt-dlp' in script, applying the 'qual' flags we defined up above.
 
-cleanup='rm -f *.temp.* *.json *.meta' # This variable makes cleanup occur after downloading.
+cleanup='rm -f *.temp.* *.json *.meta .webp' # This variable makes cleanup occur after downloading.
 
 ## Change into the YouTube directory, and begin our log, starting with when the command first executed.
 ## We nuke the old log, and rewrite it from scratch from this point, then append at the end of the script.
