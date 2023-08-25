745a0eebf7e545de6efd01df535b93e2c36f0dfa
Mr. Jacoby <etjakeoc@gmail.com>
2023-08-24 23:29:54 -0400

    modified:   PERMS.sh

--- PERMS.sh
+++ PERMS.sh
@@ -3,3 +3,9 @@ sudo chmod -R u+rwx /STORAGE/MEDIA
 sudo chmod -R g+rwx /STORAGE/MEDIA
 sudo chmod -R o-rw /STORAGE/MEDIA
 sudo chmod -R o+x /STORAGE/MEDIA
+
+sudo chown -R jake:plex /MEDIA
+sudo chmod -R u+rwx /MEDIA
+sudo chmod -R g+rwx /MEDIA
+sudo chmod -R o-rw /MEDIA
+sudo chmod -R o+x /MEDIA
