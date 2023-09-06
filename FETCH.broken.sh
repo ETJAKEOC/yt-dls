b35c6bd6b44738b6bd97ec32ffaf2178763c95cd
Mr. Jacoby <etjakeoc@gmail.com>
2023-09-05 22:42:05 -0400

    modified:   FETCH.broken.sh

--- FETCH.broken.sh
+++ FETCH.broken.sh
@@ -9,7 +9,7 @@ PATH=/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:$PATH
 ## Set YouTube download directory and 'yt-dlp' flags.
 ## These flags by default will download up to 1 video, per channel, at full quality HD, with chapter data appened, and sponsor segments removed.
 yt='/STORAGE/MEDIA/YOUTUBE' # Where the YouTube main directory is.
-yts='/STORAGE/MEDIA/YOUTUBE' # Where the YouTube script directory is.
+yts='/STORAGE/GIT/YT-DLS' # Where the YouTube script directory is.
 source $yts/SOURCE.rc
 play="--no-playlist --playlist-end 1"
 dlp_yt="yt-dlp $emb $qual $down $spon $play"
