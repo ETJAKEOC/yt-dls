65450b6d23d49ee87d1660544630a6933dfc92d7
Mr. Jacoby <etjakeoc@gmail.com>
2024-02-22 16:12:27 -0500

    modified:   FETCH.sh 	modified:   PERMS.sh

--- PERMS.sh
+++ PERMS.sh
@@ -3,3 +3,8 @@ sudo chmod -R u+rwx /MEDIA
 sudo chmod -R g+rwx /MEDIA
 sudo chmod -R o-rw /MEDIA
 sudo chmod -R o+x /MEDIA
+sudo chown -R jake:plex /STORAGE/YOUTUBE
+sudo chmod -R u+rwx /STORAGE/YOUTUBE
+sudo chmod -R g+rwx /STORAGE/YOUTUBE
+sudo chmod -R o-rw /STORAGE/YOUTUBE
+sudo chmod -R o+x /STORAGE/YOUTUBE
