b4b8b61ef2c55545efbe0cae23392be7b8b707cf
Mr. Jacoby <etjakeoc@gmail.com>
2024-05-30 02:30:10 -0400

    Unlocked the speed:   FETCH.sh

--- FETCH.sh
+++ FETCH.sh
@@ -9,9 +9,9 @@ export PATH=/usr/local/bin:/usr/local/sbin:/bin:/sbin:/usr/bin:/usr/sbin:$PATH #
 
 yt='/STORAGE/YOUTUBE' # Where the YouTube main directory is.
 yts='/STORAGE/GIT/YT-DLS' # Where the YouTube script directory is.
-fopts="--ignore-errors --no-overwrites --continue" # File related options.
+fopts="--ignore-errors --no-overwrites --continue --video-multistreams --audio-multistreams --merge-output-format mkv" # File related options.
 demb="-4 -r 200M --downloader aria2c --embed-metadata --embed-thumbnail --embed-subs --embed-chapters --sub-lang en --convert-subs=srt" # Set the downloader and what to embed into the video.
-playqual="--no-playlist --playlist-end 1 -S +res:1080,+codec:vp9:aac" # Set the playlist limit and video quality.
+playqual="--no-playlist --playlist-end 1 -S +res:best,+codec:vp9:aac" # Set the playlist limit and video quality.
 spon="--sponsorblock-remove sponsor" # Set the sponsorblock options.
 
 ids="$yt/IDs.db" # Where YT video IDs live.
@@ -37,11 +37,11 @@ echo -e "SOURCE.rc settings imported." >> $yt/LOG.txt
 cd $yt/Asianometry/ && $dlp_yt https://www.youtube.com/@Asianometry/videos &
 cd $yt/Aging\ Wheels/ && $dlp_yt https://www.youtube.com/user/agingwheels/videos &
 cd $yt/Big\ Clive/ && $dlp_yt https://www.youtube.com/c/Bigclive/videos &
-cd $yt/Blitz/ && $dlp_yt https://www.youtube.com/@Blitz/videos &
+cd $yt/Blitz/ && $dlp_yt https://www.youtube.com/@Blitz/videos & wait
 cd $yt/CRD/ && $dlp_yt https://www.youtube.com/c/@CathodeRayDude/videos &
-cd $yt/CI/ && $dlp_yt https://www.youtube.com/c/CertifiablyIngame/videos & wait
+cd $yt/CI/ && $dlp_yt https://www.youtube.com/c/CertifiablyIngame/videos &
 cd $yt/EI/ && $dlp_yt https://www.youtube.com/@ExtractionsAndIre/videos &
-cd $yt/EF/ && $dlp_yt https://www.youtube.com/@ExplosionsAndFire/videos &
+cd $yt/EF/ && $dlp_yt https://www.youtube.com/@ExplosionsAndFire/videos & wait
 cd $yt/EE/ && $dlp_yt https://www.youtube.com/@EngineeringExplained/videos &
 cd $yt/EM/ && $dlp_yt https://www.youtube.com/@EvilmonkeyzDesignz/videos &
 cd $yt/Keralis/ && $dlp_yt https://www.youtube.com/@Keralis/videos &
@@ -49,11 +49,11 @@ cd $yt/LR/ && $dlp_yt https://www.youtube.com/@rossmanngroup/videos & wait
 cd $yt/Nexpo/ && $dlp_yt https://www.youtube.com/@Nexpo/videos &
 cd $yt/PBSST/ && $dlp_yt https://www.youtube.com/@pbsspacetime/videos &
 cd $yt/PE/ && $dlp_yt https://www.youtube.com/c/@PracticalEngineeringChannel/videos &
-cd $yt/Qxir/ && $dlp_yt https://www.youtube.com/c/QxirYT/videos &
+cd $yt/Qxir/ && $dlp_yt https://www.youtube.com/c/QxirYT/videos & wait
 cd $yt/RB/ && $dlp_yt https://www.youtube.com/@RetroBytesUK/videos &
-cd $yt/RCE/ && $dlp_yt https://www.youtube.com/c/RealCivilEngineerGaming/videos & wait
+cd $yt/RCE/ && $dlp_yt https://www.youtube.com/c/RealCivilEngineerGaming/videos &
 cd $yt/RMS/ && $dlp_yt https://www.youtube.com/c/RobertMurraySmith/videos &
-cd $yt/SW/ && $dlp_yt https://www.youtube.com/c/@silentwisperer/videos &
+cd $yt/SW/ && $dlp_yt https://www.youtube.com/c/@silentwisperer/videos & wait
 cd $yt/SZS/ && $dlp_yt https://www.youtube.com/c/@SubjectZeroScience/videos &
 cd $yt/TC/ && $dlp_yt https://www.youtube.com/c/@TechnologyConnections/videos &
 cd $yt/TE/ && $dlp_yt https://www.youtube.com/@TechnologyConnextras/videos &
