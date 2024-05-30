b8b1126078d1efd018121734ccccf088e4434971
Mr. Jacoby <etjakeoc@gmail.com>
2024-05-30 02:53:08 -0400

    Removed 2 channels:   FETCH.sh

--- FETCH.sh
+++ FETCH.sh
@@ -37,19 +37,17 @@ echo -e "SOURCE.rc settings imported." >> $yt/LOG.txt
 cd $yt/Asianometry/ && $dlp_yt https://www.youtube.com/@Asianometry/videos &
 cd $yt/Aging\ Wheels/ && $dlp_yt https://www.youtube.com/user/agingwheels/videos &
 cd $yt/Big\ Clive/ && $dlp_yt https://www.youtube.com/c/Bigclive/videos &
-cd $yt/Blitz/ && $dlp_yt https://www.youtube.com/@Blitz/videos & wait
+cd $yt/Blitz/ && $dlp_yt https://www.youtube.com/@Blitz/videos &
 cd $yt/CRD/ && $dlp_yt https://www.youtube.com/c/@CathodeRayDude/videos &
-cd $yt/CI/ && $dlp_yt https://www.youtube.com/c/CertifiablyIngame/videos &
+cd $yt/CI/ && $dlp_yt https://www.youtube.com/c/CertifiablyIngame/videos & wait
 cd $yt/EI/ && $dlp_yt https://www.youtube.com/@ExtractionsAndIre/videos &
-cd $yt/EF/ && $dlp_yt https://www.youtube.com/@ExplosionsAndFire/videos & wait
+cd $yt/EF/ && $dlp_yt https://www.youtube.com/@ExplosionsAndFire/videos &
 cd $yt/EE/ && $dlp_yt https://www.youtube.com/@EngineeringExplained/videos &
-cd $yt/EM/ && $dlp_yt https://www.youtube.com/@EvilmonkeyzDesignz/videos &
-cd $yt/Keralis/ && $dlp_yt https://www.youtube.com/@Keralis/videos &
-cd $yt/LR/ && $dlp_yt https://www.youtube.com/@rossmanngroup/videos & wait
+cd $yt/LR/ && $dlp_yt https://www.youtube.com/@rossmanngroup/videos &
 cd $yt/Nexpo/ && $dlp_yt https://www.youtube.com/@Nexpo/videos &
-cd $yt/PBSST/ && $dlp_yt https://www.youtube.com/@pbsspacetime/videos &
+cd $yt/PBSST/ && $dlp_yt https://www.youtube.com/@pbsspacetime/videos & wait
 cd $yt/PE/ && $dlp_yt https://www.youtube.com/c/@PracticalEngineeringChannel/videos &
-cd $yt/Qxir/ && $dlp_yt https://www.youtube.com/c/QxirYT/videos & wait
+cd $yt/Qxir/ && $dlp_yt https://www.youtube.com/c/QxirYT/videos &
 cd $yt/RB/ && $dlp_yt https://www.youtube.com/@RetroBytesUK/videos &
 cd $yt/RCE/ && $dlp_yt https://www.youtube.com/c/RealCivilEngineerGaming/videos &
 cd $yt/RMS/ && $dlp_yt https://www.youtube.com/c/RobertMurraySmith/videos &
@@ -57,11 +55,9 @@ cd $yt/SW/ && $dlp_yt https://www.youtube.com/c/@silentwisperer/videos & wait
 cd $yt/SZS/ && $dlp_yt https://www.youtube.com/c/@SubjectZeroScience/videos &
 cd $yt/TC/ && $dlp_yt https://www.youtube.com/c/@TechnologyConnections/videos &
 cd $yt/TE/ && $dlp_yt https://www.youtube.com/@TechnologyConnextras/videos &
-cd $yt/Tech\ Ingredients/ && $dlp_yt https://www.youtube.com/c/TechIngredients/videos & wait
+cd $yt/Tech\ Ingredients/ && $dlp_yt https://www.youtube.com/c/TechIngredients/videos &
 cd $yt/TLC/ && $dlp_yt https://www.youtube.com/c/TheLinuxCast/videos &
-cd $yt/TLE/ && $dlp_yt https://www.youtube.com/c/TheLinuxExperiment/videos
-
-wait
+cd $yt/TLE/ && $dlp_yt https://www.youtube.com/c/TheLinuxExperiment/videos & wait
 
 ## Change back into the main YouTube directory, cleanup junk files, and echos a timestamp into a log file, telling us how long the script ran for.
 cd $yt && rm -rf */{*.temp.*,*.json,*.meta,*.webp}
