875ffbb28c74081e48cdb65e6ab4e0fa26251976
Mr. Jacoby <etjakeoc@gmail.com>
2024-05-30 02:54:54 -0400

    Spaced out 4 tasks for my 4 cores:   FETCH.sh

--- FETCH.sh
+++ FETCH.sh
@@ -37,11 +37,11 @@ echo -e "SOURCE.rc settings imported." >> $yt/LOG.txt
 cd $yt/Asianometry/ && $dlp_yt https://www.youtube.com/@Asianometry/videos &
 cd $yt/Aging\ Wheels/ && $dlp_yt https://www.youtube.com/user/agingwheels/videos &
 cd $yt/Big\ Clive/ && $dlp_yt https://www.youtube.com/c/Bigclive/videos &
-cd $yt/Blitz/ && $dlp_yt https://www.youtube.com/@Blitz/videos &
+cd $yt/Blitz/ && $dlp_yt https://www.youtube.com/@Blitz/videos & wait
 cd $yt/CRD/ && $dlp_yt https://www.youtube.com/c/@CathodeRayDude/videos &
-cd $yt/CI/ && $dlp_yt https://www.youtube.com/c/CertifiablyIngame/videos & wait
+cd $yt/CI/ && $dlp_yt https://www.youtube.com/c/CertifiablyIngame/videos &
 cd $yt/EI/ && $dlp_yt https://www.youtube.com/@ExtractionsAndIre/videos &
-cd $yt/EF/ && $dlp_yt https://www.youtube.com/@ExplosionsAndFire/videos &
+cd $yt/EF/ && $dlp_yt https://www.youtube.com/@ExplosionsAndFire/videos & wait
 cd $yt/EE/ && $dlp_yt https://www.youtube.com/@EngineeringExplained/videos &
 cd $yt/LR/ && $dlp_yt https://www.youtube.com/@rossmanngroup/videos &
 cd $yt/Nexpo/ && $dlp_yt https://www.youtube.com/@Nexpo/videos &
@@ -49,11 +49,11 @@ cd $yt/PBSST/ && $dlp_yt https://www.youtube.com/@pbsspacetime/videos & wait
 cd $yt/PE/ && $dlp_yt https://www.youtube.com/c/@PracticalEngineeringChannel/videos &
 cd $yt/Qxir/ && $dlp_yt https://www.youtube.com/c/QxirYT/videos &
 cd $yt/RB/ && $dlp_yt https://www.youtube.com/@RetroBytesUK/videos &
-cd $yt/RCE/ && $dlp_yt https://www.youtube.com/c/RealCivilEngineerGaming/videos &
+cd $yt/RCE/ && $dlp_yt https://www.youtube.com/c/RealCivilEngineerGaming/videos & wait
 cd $yt/RMS/ && $dlp_yt https://www.youtube.com/c/RobertMurraySmith/videos &
-cd $yt/SW/ && $dlp_yt https://www.youtube.com/c/@silentwisperer/videos & wait
+cd $yt/SW/ && $dlp_yt https://www.youtube.com/c/@silentwisperer/videos &
 cd $yt/SZS/ && $dlp_yt https://www.youtube.com/c/@SubjectZeroScience/videos &
-cd $yt/TC/ && $dlp_yt https://www.youtube.com/c/@TechnologyConnections/videos &
+cd $yt/TC/ && $dlp_yt https://www.youtube.com/c/@TechnologyConnections/videos & wait
 cd $yt/TE/ && $dlp_yt https://www.youtube.com/@TechnologyConnextras/videos &
 cd $yt/Tech\ Ingredients/ && $dlp_yt https://www.youtube.com/c/TechIngredients/videos &
 cd $yt/TLC/ && $dlp_yt https://www.youtube.com/c/TheLinuxCast/videos &
