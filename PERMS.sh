6b16205d7abf6ceb4de2850bfd36ae64a9640800
ETJAKEOC <etjakeoc@gmail.com>
2025-01-13 21:03:42 -0500

    modified:   PERMS.sh modified:   etc/yt-dlp.conf

--- PERMS.sh
+++ PERMS.sh
@@ -1,3 +1,33 @@
+sudo chown jake:plex /MEDIA
+sudo chmod u+rwx /MEDIA
+sudo chmod g+rwx /MEDIA
+sudo chmod o-rw /MEDIA
+sudo chmod o+x /MEDIA
+
+sudo chown -R root:plex /MEDIA/PMS
+sudo chmod -R u+rwx /MEDIA/PMS
+sudo chmod -R g+rwx /MEDIA/PMS
+sudo chmod -R o-rw /MEDIA/PMS
+sudo chmod -R o+x /MEDIA/PMS
+
+sudo chown -R jake:plex /MEDIA/MOVIES
+sudo chmod -R u+rwx /MEDIA/MOVIES
+sudo chmod -R g+rwx /MEDIA/MOVIES
+sudo chmod -R o-rw /MEDIA/MOVIES
+sudo chmod -R o+x /MEDIA/MOVIES
+
+sudo chown -R jake:plex /MEDIA/MUSIC
+sudo chmod -R u+rwx /MEDIA/MUSIC
+sudo chmod -R g+rwx /MEDIA/MUSIC
+sudo chmod -R o-rw /MEDIA/MUSIC
+sudo chmod -R o+x /MEDIA/MUSIC
+
+sudo chown -R jake:plex /MEDIA/TV
+sudo chmod -R u+rwx /MEDIA/TV
+sudo chmod -R g+rwx /MEDIA/TV
+sudo chmod -R o-rw /MEDIA/TV
+sudo chmod -R o+x /MEDIA/TV
+
 sudo chown -R jake:plex /MEDIA/YOUTUBE
 sudo chmod -R u+rwx /MEDIA/YOUTUBE
 sudo chmod -R g+rwx /MEDIA/YOUTUBE
