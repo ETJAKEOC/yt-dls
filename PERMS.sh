f1bb6cd224158b1404f44158b8bacdffb1c1438c
ETJAKEOC <etjakeoc@gmail.com>
2025-09-19 06:20:53 -0400

    Updates for Jellyfin since Plex wanted to kill its company

--- PERMS.sh
+++ PERMS.sh
@@ -1,29 +1,29 @@
 sudo chown jake:plex /MEDIA
 sudo chmod u+rwx /MEDIA
 sudo chmod g+rwx /MEDIA
-sudo chmod o-rw /MEDIA
-sudo chmod o+x /MEDIA
+sudo chmod o-w /MEDIA
+sudo chmod o+rx /MEDIA
 
 sudo chown -R jake:plex /MEDIA/MOVIES
 sudo chmod -R u+rwx /MEDIA/MOVIES
 sudo chmod -R g+rwx /MEDIA/MOVIES
-sudo chmod -R o-rw /MEDIA/MOVIES
-sudo chmod -R o+x /MEDIA/MOVIES
+sudo chmod -R o-w /MEDIA/MOVIES
+sudo chmod -R o+rx /MEDIA/MOVIES
 
 sudo chown -R jake:plex /MEDIA/MUSIC
 sudo chmod -R u+rwx /MEDIA/MUSIC
 sudo chmod -R g+rwx /MEDIA/MUSIC
-sudo chmod -R o-rw /MEDIA/MUSIC
-sudo chmod -R o+x /MEDIA/MUSIC
+sudo chmod -R o-w /MEDIA/MUSIC
+sudo chmod -R o+rx /MEDIA/MUSIC
 
 sudo chown -R jake:plex /MEDIA/TV
 sudo chmod -R u+rwx /MEDIA/TV
 sudo chmod -R g+rwx /MEDIA/TV
-sudo chmod -R o-rw /MEDIA/TV
-sudo chmod -R o+x /MEDIA/TV
+sudo chmod -R o-w /MEDIA/TV
+sudo chmod -R o+rx /MEDIA/TV
 
 sudo chown -R jake:plex /MEDIA/YOUTUBE
 sudo chmod -R u+rwx /MEDIA/YOUTUBE
 sudo chmod -R g+rwx /MEDIA/YOUTUBE
-sudo chmod -R o-rw /MEDIA/YOUTUBE
-sudo chmod -R o+x /MEDIA/YOUTUBE
+sudo chmod -R o-w /MEDIA/YOUTUBE
+sudo chmod -R o+rx /MEDIA/YOUTUBE
