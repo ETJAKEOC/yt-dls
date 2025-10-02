8dd8b36aba2f9dac1ff53b93bfef0fd568dcca86
ETJAKEOC <etjakeoc@gmail.com>
2025-10-02 17:38:25 -0400

    Removing faggot ass British fucktard who uses the same screaming meme too many fucking times

--- PERMS.sh
+++ PERMS.sh
@@ -1,28 +1,28 @@
-sudo chown jake:plex /MEDIA
+sudo chown jake:jellyfin /MEDIA
 sudo chmod u+rwx /MEDIA
 sudo chmod g+rwx /MEDIA
 sudo chmod o-w /MEDIA
 sudo chmod o+rx /MEDIA
 
-sudo chown -R jake:plex /MEDIA/MOVIES
+sudo chown -R jake:jellyfin /MEDIA/MOVIES
 sudo chmod -R u+rwx /MEDIA/MOVIES
 sudo chmod -R g+rwx /MEDIA/MOVIES
 sudo chmod -R o-w /MEDIA/MOVIES
 sudo chmod -R o+rx /MEDIA/MOVIES
 
-sudo chown -R jake:plex /MEDIA/MUSIC
+sudo chown -R jake:jellyfin /MEDIA/MUSIC
 sudo chmod -R u+rwx /MEDIA/MUSIC
 sudo chmod -R g+rwx /MEDIA/MUSIC
 sudo chmod -R o-w /MEDIA/MUSIC
 sudo chmod -R o+rx /MEDIA/MUSIC
 
-sudo chown -R jake:plex /MEDIA/TV
+sudo chown -R jake:jellyfin /MEDIA/TV
 sudo chmod -R u+rwx /MEDIA/TV
 sudo chmod -R g+rwx /MEDIA/TV
 sudo chmod -R o-w /MEDIA/TV
 sudo chmod -R o+rx /MEDIA/TV
 
-sudo chown -R jake:plex /MEDIA/YOUTUBE
+sudo chown -R jake:jellyfin /MEDIA/YOUTUBE
 sudo chmod -R u+rwx /MEDIA/YOUTUBE
 sudo chmod -R g+rwx /MEDIA/YOUTUBE
 sudo chmod -R o-w /MEDIA/YOUTUBE
